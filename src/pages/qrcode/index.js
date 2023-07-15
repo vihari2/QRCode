f8707f396fcc6d1331a37657a5615a30350e09f7
Vitoria <freitasvitoria712@gmail.com>
2023-07-15 18:17:25 -0300

    [add] pastas

--- src/pages/qrcode/index.js
+++ src/pages/qrcode/index.js
@@ -1,7 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "../../logo.svg";
+import "./qrcode.css";
 
-function App() {
+function Qrcode() {
   return (
     <div className="App">
       <header className="App-header">
@@ -22,4 +22,4 @@ function App() {
   );
 }
 
-export default App;
+export default Qrcode;
